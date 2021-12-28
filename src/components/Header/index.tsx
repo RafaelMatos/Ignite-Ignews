e03264240b16b8334ce7e5bc8600df1e5e72881c
rafaelibiapina <rafaelibiapina@sefaz.pi.gov.br>
2021-12-28 17:35:46 -0300

    first commit

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,9 @@
+export function Header(){
+    return(
+        <header>
+            <div>
+                <img src="/images/" alt="" />
+            </div>
+        </header>
+    )
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,16 @@
+// import styles from '../styles/home.module.scss'
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <>
+    <Head>
+      <title>Inicio | Ig.news</title>
+    </Head>
+    <h1>
+      
+      Hello World!
+    </h1>
+    </>
+  )
+}
