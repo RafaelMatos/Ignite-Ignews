6cd5358b0e094415c9c44fcce1b3368de8c6dcab
RafaelMatos <rafael-matos@outlook.com>
2022-01-19 08:53:48 -0300

    creating checkout session

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,10 +1,27 @@
+import { useSession,signIn,signOut } from 'next-auth/react'
 import styles from './styles.module.scss'
 
 interface SubscribeButtonProps {
     priceId : string;
 }
 
+//getServerSideProps (SSR)
+//getStaticProps (SSG)
+//API routes
+
 export function SubscribeButton( { priceId } : SubscribeButtonProps){
+
+    const { data : session } = useSession()
+
+    function handleSubscriibe( ) {
+        if(!session){
+            signIn('github')
+            return;
+        }
+
+        //criação da checkout session
+        
+    }
 
     return(
         <button
