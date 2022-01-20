fa0b8a51f6fd30741ef857a055404612fb67b5e3
RafaelMatos <rafael-matos@outlook.com>
2022-01-20 13:29:24 -0300

    Saving customer on Stripe

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,4 +1,6 @@
 import { useSession,signIn,signOut } from 'next-auth/react'
+import { api } from '../../services/api';
+import { getStripeJs } from '../../services/stripe-js';
 import styles from './styles.module.scss'
 
 interface SubscribeButtonProps {
@@ -13,20 +15,33 @@ export function SubscribeButton( { priceId } : SubscribeButtonProps){
 
     const { data : session } = useSession()
 
-    function handleSubscriibe( ) {
+    async function handleSubscribe( ) {
         if(!session){
             signIn('github')
             return;
         }
+        try{
+            const response = await api.post('/subscribe')
+
+            const { sessionId } = response.data
+
+            const stripe = await getStripeJs()
+
+            await stripe.redirectToCheckout({sessionId:sessionId})
+
+        } catch(err){
+            alert(err.message)
+        }
 
         //criação da checkout session
-        
+
     }
 
     return(
         <button
             type="button"
             className={styles.subscribeButton}
+            onClick={handleSubscribe}
         >
             Subscribe Now!
         </button>
